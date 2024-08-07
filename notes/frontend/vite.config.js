66c9f47bc6640f648b677e0a8f1fa19f4d7d331f
anoying-kid <yugrana853@gmail.com>
2024-08-07 17:46:38 +0530

    new files for docker

--- notes/frontend/vite.config.js
+++ notes/frontend/vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
