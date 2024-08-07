3cdced901e4fef65dc0bb2fbe15ce8d7243c2d3e
anoying-kid <yugrana853@gmail.com>
2024-08-07 17:01:01 +0530

    dockerfile

--- notes/frontend/vite.config.js
+++ notes/frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: '../backend/staticfiles',
-    emptyOutDir: true,
+    outDir: 'dist',
   },
-};
+})
