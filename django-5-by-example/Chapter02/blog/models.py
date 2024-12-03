76d17d97bf2c2e2d655d66d9ffffbdee79514de2
anoying-kid <yugrana853@gmail.com>
2024-12-03 21:13:45 +0530

    Added canoical url in the model

--- django-5-by-example/Chapter02/blog/models.py
+++ django-5-by-example/Chapter02/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 from django.utils import timezone
 
 class PublishManager(models.Manager):
@@ -39,3 +40,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
