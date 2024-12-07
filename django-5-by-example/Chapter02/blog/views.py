5a68e78cede81e0c390e2ef4cdfbc2d2ec44671a
anoying-kid <yugrana853@gmail.com>
2024-12-08 00:39:05 +0530

    Added the Form and views for the email sending feature

--- django-5-by-example/Chapter02/blog/views.py
+++ django-5-by-example/Chapter02/blog/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from .forms import EmailPostForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -25,3 +25,23 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post':post},
     )
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+    
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            print("Email Sent")
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+    )
+    
