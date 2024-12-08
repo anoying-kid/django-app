c5017bc548fe3cf8bb134ed76ecb1cdca9c2e615
anoying-kid <yugrana853@gmail.com>
2024-12-08 21:12:26 +0530

    Completed the sharing blog part using email server

--- django-5-by-example/Chapter02/blog/urls.py
+++ django-5-by-example/Chapter02/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, post_detail
+from .views import PostListView, post_detail, post_share
 
 app_name = 'blog'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('', post_list, name='post_list'),
     path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share'),
 ]
